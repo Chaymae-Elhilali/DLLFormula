30eb577b46aa211b768b53ed6255336635208cd7
Chaymae-Elhilali <91722533+Chaymae-Elhilali@users.noreply.github.com>
2023-06-02 16:26:48 +0100

    Create remove_doubles.py

---
+++ remove_doubles.py
@@ -0,0 +1,44 @@
+import hashlib
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import os
+
+def file_hash(filename):
+    with open(filename, 'rb') as f:
+        return hashlib.md5(f.read()).hexdigest()
+
+os.getcwd()
+os.chdir('/content/test_fct_remove_dbles')
+os.getcwd()
+
+files_list = os.listdir('.')
+print(len(files_list))
+
+duplicates = []
+hash_keys = dict()
+for index, filename in enumerate(os.listdir('.')):
+    if os.path.isfile(filename):
+        filehash = file_hash(filename)
+        if filehash not in hash_keys:
+            hash_keys[filehash] = index
+        else:
+            duplicates.append((index, hash_keys[filehash]))
+
+print(duplicates)
+
+for file_indexes in duplicates[:30]:
+    try:
+        plt.subplot(121), plt.imshow(np.array(Image.open(files_list[file_indexes[1]])))
+        plt.title(file_indexes[1]), plt.xticks([]), plt.yticks([])
+
+        plt.subplot(122), plt.imshow(np.array(Image.open(files_list[file_indexes[0]])))
+        plt.title(str(file_indexes[0]) + ' duplicate'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    except OSError as e:
+        continue
+
+for index in duplicates:
+    os.remove(files_list[index[0]])
